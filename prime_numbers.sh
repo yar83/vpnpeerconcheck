d475419dd747424389bbee5f6bacc58cb61e25c0
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-12-21 21:33:59 +0300

    feat(prime-nums): add boilerplate for script

---
+++ prime_numbers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+######################################################################
+# Represent entered number as a product of prime numbers, up to the
+# order of factors. For example, N = 1164 = 2 * 2 * 3 * 97
+######################################################################
+
+# TODO(yar83): add help
+# TODO(yar83): add argument parser to parse -h or --help to print help
+# TODO(yar83): prove entered whether -h/--help or one integer number
+
+main() {
+
+}
+
+main "$@"
