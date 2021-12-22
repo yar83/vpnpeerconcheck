865bc48defc3740a6dc7ba48060b27999dc35d98
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-12-22 23:03:32 +0300

    feat(prime-numbers): check input in standalone function

--- prime_numbers.sh
+++ prime_numbers.sh
@@ -9,8 +9,8 @@
 # TODO(yar83): prove entered whether -h/--help or one integer number
 
 input_check() {
-  if [[ ! (${#@} -eq 1 && $1 =~ '-h' || $1 =~ ^[1-9]{1}[0-9]{,9}$) ]]; then
-    echo "Valid input is one integer number greater than 0"
+  if [[ ! (${#@} -eq 1 && $1 == '-h' || $1 == '--help' || $1 =~ ^[1-9]{1}[0-9]{,9}$) ]]; then
+    echo "Valid input is one integer number greater than 0 or -h or --help for help"
     exit 1
   fi
 }
