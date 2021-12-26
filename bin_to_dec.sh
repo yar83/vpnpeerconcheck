c6b58ae2ebca48a4eb58fd15bd3dc73d0ee14864
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-12-26 19:35:09 +0300

    feat(bin_to_dec): raw version of converter

--- bin_to_dec.sh
+++ bin_to_dec.sh
@@ -7,6 +7,12 @@ set -o nounset
 set -o errexit
 
 main() {
+  len=${#1}
+  out=0
+  for ((i=0; i < $len; i++)); do
+    out=$((out + $((${1:$((len-len+i)):1} * 2 ** $((len-i-1))))))
+  done
+  echo "$out"
 }
 
 main "$@"
