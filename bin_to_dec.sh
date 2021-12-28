eef14e11ed6138e8abf1683068e20a79d591f382
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-12-28 11:08:37 +0300

    feat(bin_to_dec): add descr to funcs, move converter to standalone func

--- bin_to_dec.sh
+++ bin_to_dec.sh
@@ -6,6 +6,16 @@
 set -o nounset
 set -o errexit
 
+######################################################################
+# Validate script argument. It's valid if it's a interger decimal
+# number or -h or --help string.
+# Globals:
+#   none
+# Arguments:
+#   $@ as all arguments passed to script
+# Outputs:
+#   return 0 when argument is valid otherwise 1
+######################################################################
 input_check() {
   if [[ ${#@} -eq 1 ]] && [[ $1 =~ (^-h$)|(^--help$)|(^1{1}[0-1]{0,19}$) ]]; then
     return 0
@@ -15,18 +25,47 @@ input_check() {
 }
 
 print_error() {
-  printf "Valid input is one positive binary number\n"
+  printf "Valid input is one positive binary number or -h or --help for help\n"
+}
+
+######################################################################
+# Print help if script run with -h or --help argument
+# Globals:
+#   none
+# Arguments: 
+#   none
+# Outputs:
+#   Print to stdout help message and exit.
+######################################################################
+print_help() {
+  printf "%s\n%s\n%s\n",\
+    "Convert entered number from binary to decimal."\
+    "Use $./dec_to_bin.sh 111101010101 for normal use or enter arguments"\
+    "-h, --help to dispaly this help and exit."
+}
+
+######################################################################
+# Get decimal number from converted binary.
+# Globals:
+#   None
+# Arguments:
+#   $1 as binary number
+# Outputs:
+#   Print decimal number to stdout
+###################################################################### 
+get_decimal() {
+  len=${#1}
+  out=0
+  for ((i = 0; i < $len; i++)); do
+    out=$((out + $((${1:$((len-len+i)):1} * 2 ** $((len-i-1))))))
+  done
+  echo "$out"
 }
 
 main() {
   input_check "$@" || { print_error; exit 1; }
-
-  len=${#1}
-  out=0
-  for ((i=0; i < $len; i++)); do
-    out=$((out + $((${1:$((len-len+i)):1} * 2 ** $((len-i-1))))))
-  done
-  echo "$out"
+  
+  get_decimal "$@"
 }
 
 main "$@"
