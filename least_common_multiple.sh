c877350a61de809013817a050885ceb71a7e13fa
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-01-07 19:47:50 +0300

    feat(lcm): check_input function'

--- least_common_multiple.sh
+++ least_common_multiple.sh
@@ -3,9 +3,24 @@
 set -o nounset
 set -o errexit
 
+check_input() {
+  valid_number_arg='(^-h$)|(^--help$)|(^[1-9]{1}[0-9]{0,9}$)'
+  valid_help_arg='(^-h$)|(^--help$)'
+  if [[ $# -eq 2 ]] && [[ $1 =~ $valid_number_arg ]] && [[ $2 =~ $valid_number_arg ]]; then
+    return 0
+  fi
+
+  if [[ $# -eq 1 ]] && [[ $1 =~ $valid_help_arg ]]; then
+    return 0
+  fi
+
+  return 1
+}
+
 main() {
-  echo "$1"
-  echo "$2"
+  check_input "$@" || { echo "error"; exit 1; }
+
+  echo "Input valid"
 }
 
 main "$@"
