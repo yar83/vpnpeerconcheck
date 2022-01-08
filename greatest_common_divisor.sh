784016f008bef7886cfdaf4fe20f8d0399c66886
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-01-08 18:20:07 +0300

    feat(GCD): add check_input func

--- greatest_common_divisor.sh
+++ greatest_common_divisor.sh
@@ -2,9 +2,22 @@
 set -o nounset
 set -o errexit
 
+check_input() {
+  valid_number_arg='^[1-9]{1}[0-9]{0,9}$'
+  valid_help_arg='(^-h$)|(^--help$)'
+  if [[ $# -eq 2 ]] && [[ $1 =~ $valid_number_arg ]] && [[ $2 =~ $valid_number_arg ]]; then
+    return 0
+  fi
+
+  if [[ $# -eq 1 ]] && [[ $1 =~ $valid_help_arg ]]; then
+    return 0
+  fi
+
+  return 1
+}
+
 main() {
-  echo "$1"
-  echo "$2"
+
 }
 
 main "$@"
